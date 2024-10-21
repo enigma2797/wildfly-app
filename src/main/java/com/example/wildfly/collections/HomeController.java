348c5fc15087d88f17ca6c41b0179729eba3111c
Saurav Raj <sraj@q3infotech.net>
2024-10-21 11:01:48 +0100

    changes for db and hikari

---
+++ src/main/java/com/example/wildfly/collections/HomeController.java
@@ -0,0 +1,39 @@
+package com.example.wildfly.collections;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.wildfly.entities.Product;
+import com.example.wildfly.repositories.ProductRepository;
+
+@RestController
+public class HomeController {
+	
+	@Autowired
+	private ProductRepository productRepo;
+	@GetMapping("/home")
+	public String home()
+	{
+		return "home";
+	}
+	
+	@GetMapping("/")
+	public String root()
+	{
+		return "hi from wildfly";
+	}
+	
+	@GetMapping("/products")
+	public ResponseEntity<List<Product>>getAllProducts()
+	{
+		List<Product>products=productRepo.findAll();
+		return ResponseEntity.ok(products);
+	}
+	
+	
+
+}
---
+++ src/main/java/com/example/wildfly/entities/Product.java
@@ -0,0 +1,41 @@
+package com.example.wildfly.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="product")
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private Double price;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Product() {
+		super();
+	}
+	
+	
+
+}
